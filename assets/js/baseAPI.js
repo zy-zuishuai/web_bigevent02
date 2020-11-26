ace63f8db21dec9592dc4f9f0c1df44ffbd3ab73
itheima <itheim@itcast.cn>
2020-11-26 20:19:22 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,25 @@ $.ajaxPrefilter(function(params){
     //拼接对应环境的服务器地址
     params.url= baseUPL + params.url
     //alert(params.url);
-})
+
+
+
+
+
+
+    if(params.url.indexOf("/my/") !== -1){
+        params.headers = {
+            Authorization: localStorage.getItem('token') || ""
+        }
+    }
+
+
+    params.complete =function(res){
+        console.log(res.responseJSON);
+        var obj = res.responseJSON;
+        if(obj,status ==1 && obj.message =="身份认证失败!"){
+           localStorage.removeItem("token");
+           location.href = '/login.html'  
+        }
+    }
+});
